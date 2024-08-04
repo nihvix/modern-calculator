f665db0c3062476098538fdad55ea298c30d3d3a
Inma Quesada Tirado <nihvix.art@gmail.com>
2024-08-04 13:47:07 +0200

    Digit buttons

--- app.js
+++ app.js
@@ -1,12 +1,41 @@
 /* ====================
-        FUNCIONES
+        VARIABLES
    ==================== */
+var inputOperation;
+var result;
+var item;
+var text = "";
+
+/* ====================
+     FUNCIONES
+==================== */
+/**
+ * Función para añadir el texto del botón a la pantalla
+ * @param {Event} event 
+ */
+function addText(event) {
+     item = event.target;
+     text += "" + item.innerText;
+     console.log(text);
+     result.innerText = text;
+}
 
 /* ========================
      EVENTO PRINCIPAL
    ======================== */
+function DOMLoaded(event) {
+     inputOperation = document.getElementById('userInput');
+     result = document.getElementById('result');
 
+     //Añadimos el listener a los botones numéricos
+     let digitButtons = document.getElementsByClassName('button');
+     for (let btn of digitButtons) {
+          btn.addEventListener('mousedown', addText);
+     }
+
+
+}
 /* ===========================
         LLAMADA PRINCIPAL
    =========================== */
-
+document.addEventListener('DOMContentLoaded', DOMLoaded);
